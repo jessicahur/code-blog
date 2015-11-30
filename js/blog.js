02fb5576bc91e1cc96064692d0d442d82b29934a
Jessica Tran Hur <jesshur824@gmail.com>
2015-11-30 13:08:18 -0800

    added jQuery-min, added function constructor to create article objects

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,21 @@
-var jessBlog = jessBlog ||{
+$(function(){
+  //Declare blog obj variable
+  var blog = blog ||{};
 
-  article: function (title, category, author, authorURL, timeStamp, content){
+  //Contructor for article objects
+  function article(title, category, author, authorURL, publishedOn, body){
     this.title = title;
     this.category = category;
     this.author = author;
     this.authorURL = authorURL;
-    this.timeStamp = timeStamp;
-    this.content = content;
-  },
+    this.timeStamp = publishedOn;
+    this.content = body;
+  };
 
+  //function to insert article obj content into index.html
+  blog.makeArticle = function (article){
+    var blogAr = $('#articles').clone();
+    blogAr.find('h1').text(article.title);
+  };
 
-
-};
+});
