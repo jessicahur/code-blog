9412cfafdc72260271cfc6ad7e0cd066ad59ed02
Jessica Tran Hur <jesshur824@gmail.com>
2015-11-30 14:54:10 -0800

    working blog so far

--- js/blog.js
+++ js/blog.js
@@ -8,26 +8,48 @@ $(function(){
     this.author = obj.author;
     this.authorURL = obj.authorURL;
     this.timeStamp = obj.publishedOn;
-    this.content = obj.body;
+    this.body = obj.body;
   };
 
   //function to insert article obj content into index.html
   article.prototype.toHtml = function (){
-    var $blogAr = $('#articles').clone();
+    $blogAr = $('.arTemplate').clone();
+    $blogAr.removeClass('arTemplate');
     $blogAr.find('h1').text(this.title);
     $blogAr.find('.author').text(this.author);
     $blogAr.find('.URL').text(this.authorUrl);
     $blogAr.find('.category').text(this.category);
-    $blogAr.find('time').text('exactly ' + parseInt((new Date() - new Date(this.timeStamp))/60/60/24/1000) + ' days ago');
+    $blogAr.find('time').text('published on: '+this.timeStamp);
+    $blogAr.find('.article-body').html(this.body);
+    return $blogAr;
   };
 
   //function to go through blogArticles.js, pick out article, create objs, sort and call toHTML()
   blog.makeArticle = function (){
-    var articlesArray=[];
+    var tempArticlesArray = [];
+    var articlesArray = [];
+    var timeStampArray = [];
+    //creating article obj and push them in tempArticlesArray
     for (var ii =0; ii<this.rawData.length; ii++){
       var newArticle = new article(this.rawData[ii]);
-      articlesArray.push(newArticle);
+      tempArticlesArray.push(newArticle);
+      timeStampArray.push(this.rawData[ii].publishedOn);
     }
-
+    timeStampArray.sort();
+    //sorting articles based on sorted timeStamp array
+    for (var jj=0; jj<timeStampArray.length;jj++){
+      for (var kk=0; kk<tempArticlesArray.length; kk++){
+        if (timeStampArray[jj]===tempArticlesArray[kk].timeStamp){
+          articlesArray.push(tempArticlesArray[kk]);
+          tempArticlesArray.splice(kk,1);
+          break;
+        }
+      }
+    }
+    //Inserting articles into blog
+    for (var nn=0; nn<articlesArray.length; nn++){
+      $('#articles').prepend(articlesArray[nn].toHtml());
+    }
   };
+  blog.makeArticle();
 });
