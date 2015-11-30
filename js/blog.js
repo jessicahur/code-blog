fcee5d7be39ac8c4d2d6c4dce989515dac0613bf
Jessica Tran Hur <jesshur824@gmail.com>
2015-11-29 20:50:21 -0800

    add article function contrsutor to the blog obj

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,14 @@
 var jessBlog = jessBlog ||{
 
+  article: function (title, category, author, authorURL, timeStamp, content){
+    this.title = title;
+    this.category = category;
+    this.author = author;
+    this.authorURL = authorURL;
+    this.timeStamp = timeStamp;
+    this.content = content;
+  },
+
 
 
 };
