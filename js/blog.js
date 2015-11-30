f1bdb39ef59206893220ad82deb23592e60d2313
Jessica Tran Hur <jesshur824@gmail.com>
2015-11-30 13:25:56 -0800

    finished prototype.toHtml(), untested

--- js/blog.js
+++ js/blog.js
@@ -13,12 +13,17 @@ $(function(){
   };
 
   //function to insert article obj content into index.html
-  article.prototype.toHtml = function (article){
+  article.prototype.toHtml = function (){
     var $blogAr = $('#articles').clone();
-    $blogAr.find('h1').text(article.title);
-    $blogAr.find('.author').text(article.author);
-    $blogAr.find('.URL').text(article.authorURL);
-    $blogAr.find('.category').text(article.category);
+    $blogAr.find('h1').text(this.title);
+    $blogAr.find('.author').text(this.author);
+    $blogAr.find('.URL').text(this.authorURL);
+    $blogAr.find('.category').text(this.category);
+    $blogAr.find('time').text('exactly ' + parseInt((new Date() - new Date(this.timeStamp))/60/60/24/1000) + ' days ago');
   };
 
+  //function to go through blogArticles.js, pick out article, create objs, sort and call toHTML()
+  blog.makeArticle = function (){
+
+  };
 });
