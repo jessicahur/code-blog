d88710c7b443d814a1418037127364e0ad2565ce
Jessica Tran Hur <jesshur824@gmail.com>
2015-11-30 13:58:37 -0800

    Working article construction and partially working blog.makeArticle()--Up to making article obj from blog.rawData and push them inside articlesArray

--- js/blog.js
+++ js/blog.js
@@ -1,15 +1,14 @@
 
   //Declare blog obj variable
-var blog = blog ||{};
 $(function(){
   //Contructor for article objects
-  function article(title, category, author, authorURL, publishedOn, body){
-    this.title = title;
-    this.category = category;
-    this.author = author;
-    this.authorURL = authorURL;
-    this.timeStamp = publishedOn;
-    this.content = body;
+  function article(obj){
+    this.title = obj.title;
+    this.category = obj.category;
+    this.author = obj.author;
+    this.authorURL = obj.authorURL;
+    this.timeStamp = obj.publishedOn;
+    this.content = obj.body;
   };
 
   //function to insert article obj content into index.html
@@ -17,15 +16,18 @@ $(function(){
     var $blogAr = $('#articles').clone();
     $blogAr.find('h1').text(this.title);
     $blogAr.find('.author').text(this.author);
-    $blogAr.find('.URL').text(this.authorURL);
+    $blogAr.find('.URL').text(this.authorUrl);
     $blogAr.find('.category').text(this.category);
     $blogAr.find('time').text('exactly ' + parseInt((new Date() - new Date(this.timeStamp))/60/60/24/1000) + ' days ago');
   };
 
   //function to go through blogArticles.js, pick out article, create objs, sort and call toHTML()
   blog.makeArticle = function (){
+    var articlesArray=[];
     for (var ii =0; ii<this.rawData.length; ii++){
-
+      var newArticle = new article(this.rawData[ii]);
+      articlesArray.push(newArticle);
     }
+
   };
 });
