3ec08cbebe8a6406e38060fcf1b9d8114034a5e7
Jessica Tran Hur <jesshur824@gmail.com>
2015-12-03 21:57:42 -0800

    successfully restrain padding and height when resizing

--- js/styling.js
+++ js/styling.js
@@ -7,7 +7,7 @@ $(function(){
   console.log('window width is '+width);
   if(width<=480){
     var cw = $('.l-header').width()*2/3;
-    $('.l-header').css('height',cw+'px');
+    $lHeader.css('height',cw+'px');
     $menu.hide();
   }
   $('#menuIcon').on('click', function(){
@@ -17,6 +17,7 @@ $(function(){
   $(window).on('resize',function(){
     width = $(window).width();
     if(width<=480){
+      $listNavigation.children().css('display','block');
       console.log('menuIcon siblings are '+$menu);
       var cw = $lHeader.width()*2/3;
       $lHeader.css('height',cw+'px');
@@ -24,8 +25,16 @@ $(function(){
       //Event listener to animate menu bar
     }
     else{
+      $lHeader.css('height','50');
+      $listNavigation.css('height','auto');
+      $listNavigation.children().css({
+        'display':'inline-block',
+        'padding-left':'2em',
+        'padding-right':'2em'
+      });
       $menu.show();
       console.log('1');
+
     }
   });
 });
