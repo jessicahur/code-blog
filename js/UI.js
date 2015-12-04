44bd41525e5685f6f9f082293bd83bb680201aa4
Jessica Tran Hur <jesshur824@gmail.com>
2015-12-04 14:50:49 -0800

    Added event listeners, live updates

--- js/UI.js
+++ js/UI.js
@@ -1,12 +1,11 @@
 $(function(){
+  var $author = $('#author');
+  var $authorUrl = $('#authorUrl');
+  var $title = $('#title');
+  var $articleBody = $('#articleBody');
+  var $category = $('#category');
 
   function render(){
-    var $author = $('#author');
-    var $authorUrl = $('#authorUrl');
-    var $title = $('#title');
-    var $articleBody = $('#articleBody');
-    var $category = $('#category');
-
     var userInput = [$author, $authorUrl, $title, $articleBody, $category];
     var userInputVal = [];
     for(var ii=0; ii<userInput.length; ii++){
@@ -18,30 +17,31 @@ $(function(){
   }
 
   function insert(){
-    var $author = $('#insert-author');
-    var $authorUrl = $('#insert-url');
-    var $title = $('#insert-title');
-    var $articleBody = $('#insert-article');
-    var $category = $('#insert-category');
+    console.log('1');
+    var $authorInsert = $('#insert-author');
+    var $authorUrlInsert = $('#insert-url');
+    var $titleInsert = $('#insert-title');
+    var $articleBodyInsert = $('#insert-article');
+    var $categoryInsert = $('#insert-category');
+
     var userInputVal = render();
-
-    $author.text(userInputVal[0]);
-    $authorUrl.text(userInputVal[1]);
-    $title.text(userInputVal[2]);
-    $articleBody.append($(userInputVal[3]));
-    $category.text(userInputVal[4]);
-
+    console.log(userInputVal);
+    $articleBodyInsert.children().remove();
+    $authorInsert.text(userInputVal[0]);
+    $authorUrlInsert.text(userInputVal[1]);
+    $titleInsert.text(userInputVal[2]);
+    $articleBodyInsert.append($(userInputVal[3]));
+    $categoryInsert.text(userInputVal[4]);
   }
 
 
 
 
-
-
-
-
-
-
-
-
+  console.log($author);
+  $author.on('input', insert);
+  $authorUrl.on('input', insert);
+  $title.on('input', insert);
+  $articleBody.on('input', insert);
+  $category.on('input', insert);
+  insert();
 });
