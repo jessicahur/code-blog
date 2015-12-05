705d72bc4916037892b64711baf58ad0a8af4a5e
Jessica Tran Hur <jesshur824@gmail.com>
2015-12-04 16:27:33 -0800

    Working publish date

--- js/UI.js
+++ js/UI.js
@@ -5,7 +5,8 @@ $(function(){
   var $articleBody = $('#articleBody');
   var $category = $('#category');
   var $submitButton = $('#entry-submit');
-  var myObj = {};
+  var $timeInsert = $('#insert-time');
+  var myObj = {publishedOn: ''};
 
   function render(){
     var userInput = [$author, $authorUrl, $title, $articleBody, $category];
@@ -30,6 +31,7 @@ $(function(){
     var $titleInsert = $('#insert-title');
     var $articleBodyInsert = $('#insert-article');
     var $categoryInsert = $('#insert-category');
+
     var $pJson = $('#json-output');
     var userInputVal = render()[1];
     console.log(render()[0]);
@@ -59,11 +61,17 @@ $(function(){
   $title.on('input', insert);
   $articleBody.on('input', insert);
   $category.on('input', insert);
+
+
   $submitButton.on('click', function(e){
     e.preventDefault();
     var date = getDate();
+    date = date.replace('"', '');
     myObj.publishedOn = date;
+    $timeInsert.text(date);
     insert();
   });
+
+
   insert();
 });
