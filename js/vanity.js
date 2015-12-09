ee0d7603efc7a918ac8a01d10f1319dfdc25616f
Jessica Tran Hur <jesshur824@gmail.com>
2015-12-08 22:35:12 -0800

    working version, no styling yet

--- js/vanity.js
+++ js/vanity.js
@@ -1,7 +1,32 @@
-var blogObj;
 $(function(){
+  var get_ajax = function(){
+    return $.ajax({
+      type: 'HEAD',
+      url: 'js/blogArticles.json',
+    });//end of $.ajax
+  };
+
+  var get_json = function(){
+    sessionStorage.setItem('eTag',eTag);
+    $.getJSON('js/blogArticles1.json', function(articlesData){
+      console.log(accumulateInfo(articlesData));
+      sessionStorage.setItem('vanityStats',JSON.stringify(accumulateInfo(articlesData)));
+    });//end of $.getJson
+  };//end of get_json
+
+  var fillInTemplate = function(){
+    var vanityStats = JSON.parse(sessionStorage.getItem('vanityStats'));
+    console.log(vanityStats);
+    var rawScriptTemplate = $('#handlebarsTemplate').html();
+    var compiledScriptTemplate = Handlebars.compile(rawScriptTemplate);
+    var $statsSection = $('#blogStats');
+    var html = compiledScriptTemplate(vanityStats);
+    $statsSection.append(html);
+  };
+
   function articleWordCount(article){
-    var articleBodyWordCount = $(article.body).text().split(' ').length;
+    var articleBodyWordCount = $(article.body).text().split(/\s+/).length;
+    // console.log(articleBodyWordCount);
     return articleBodyWordCount;
   };
   function convertMarkdown(elem){
@@ -16,16 +41,18 @@ $(function(){
   function wordLength(word){
     return word.length;
   }
-  function articleTotalWordLength(article){
-    var articleBodyWordSplit = $(article.body).text().split(' ');
+  function articleAvgWordLength(article){
+    var articleBodyWordSplit = $(article.body).text().split(/\s+/);
     var articleWordLengthArray = articleBodyWordSplit.map(wordLength);
-    return Math.round(articleWordLengthArray.reduce(sum)/articleWordLengthArray.length);
+    return articleWordLengthArray.reduce(sum)/articleWordLengthArray.length;
   }
-  function makeAuthorObj (authorName){
+  function makeAuthorObj (authorName, authorAvgWordLength){
     var authorObj = {};
-    authorObj.authorName = authorName;
+    authorObj.author = authorName;
+    authorObj.avgWordLength = authorAvgWordLength;
     return authorObj;
   }
+
   function accumulateInfo(articlesData){
     var vanity = vanity || {};
     var articles = articlesData;
@@ -33,29 +60,70 @@ $(function(){
     var articlesWordsCount;
     var totalWordLength;
     var authorNames = Util.uniqueItem(articles, 'author');
-    var authorsObjArray = authorNames.map(makeAuthorObj);
-    console.log(authorNames);
-    console.log(authorsObjArray);
     vanity.numArticles = articles.length;
-    console.log(vanity.numArticles);
     vanity.numAuthors = authorNames.length;
-    console.log(vanity.numAuthors);
+    //Counting total words across all articles
     articlesWithAllHTML = articlesData.map(convertMarkdown);
-    console.log(articlesWithAllHTML);
-    articlesWordsCount = articlesWithAllHTML.map(articleWordCount);
-    console.log(articlesWordsCount);
-    vanity.totalArticlesWordsCount = articlesWordsCount.reduce(sum);
-    console.log(vanity.totalArticlesWordsCount);
-    totalWordLength = articlesWithAllHTML.map(articleTotalWordLength);
-    console.log(totalWordLength);
-    vanity.avgWordLength = Math.round(totalWordLength.reduce(sum)/(totalWordLength.length));
+    articlesWordsCountArray = articlesWithAllHTML.map(articleWordCount);
+    vanity.totalWordsCount = articlesWordsCountArray.reduce(sum);
+    //Calculating avg word length across all articles
+    avgWordLengths = articlesWithAllHTML.map(articleAvgWordLength);
+    console.log(avgWordLengths);
+    vanity.avgWordLength = Math.round(avgWordLengths.reduce(sum)/(avgWordLengths.length));
     console.log(vanity.avgWordLength);
-
-
+    //Calculating avg word length for each author
+    vanity.authorAndAvgWordLength = [];
+    var authorsWithMatchedArticles = [];
+    authorNames.forEach(function (author){
+      var authorToMatch = author;
+      var matchedArticles = articlesWithAllHTML.filter(function(article){
+        if(article.author==authorToMatch){
+          return true;
+        }
+        else{
+          return false;
+        }
+      });
+      authorsWithMatchedArticles.push(matchedArticles);
+    });
+    console.log(authorsWithMatchedArticles);
+    authorsWithMatchedArticles.forEach(function (authorWithMatchedArticles){
+      var authorName = authorWithMatchedArticles[0].author;
+      console.log(authorName);
+      authorWordLengthArray = authorWithMatchedArticles.map(articleAvgWordLength);
+      console.log(authorWordLengthArray);
+      authorAvgWordLength = Math.round(authorWordLengthArray.reduce(sum)/(authorWordLengthArray.length)*100)/100;
+      console.log(authorAvgWordLength);
+      vanity.authorAndAvgWordLength.push(makeAuthorObj(authorName,authorAvgWordLength));
+    });
+    console.log(vanity.authorAndAvgWordLength);
     return vanity;
   };
 
-  $.getJSON('js/blogArticles1.json', function(articlesData){
-    accumulateInfo(articlesData);
-  });//end of getJson
+  get_ajax().done(function(data,textStatus,xhr){
+    console.log(textStatus);
+    eTag = xhr.getResponseHeader('eTag');
+    console.log(eTag);
+    sessionStorageETag = sessionStorage.getItem('eTag');
+    if(sessionStorageETag){
+      if(sessionStorageETag!==eTag){
+        console.log('cache miss');
+        get_json();
+      }
+      else{
+        console.log('cache hit');
+        fillInTemplate();
+      }
+    }
+      else{
+        console.log('cache miss');
+        get_json();
+      }
+  });
+
+  //
+  //
+  //   }
+  // });//end of chaining get_ajax
+
 });//end of ready
